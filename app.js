08ad9976669fc389ba24ab5ee1d3a707bf654a8d
Yashkumar Senma <146329567+yashkumar1508@users.noreply.github.com>
2024-08-11 15:13:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl)
+})
+
+
+
+function showToast()
+{
+    toastList[0].show()
+}
+
+// enable tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+function goToProductDetails()
+{
+    window.location.href="/Users/singa/OneDrive/Desktop/ecommerce-bootstrap/product.html";
+}
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
